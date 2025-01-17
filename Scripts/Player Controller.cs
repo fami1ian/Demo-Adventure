89499a93e171e7913927fea36d41c4cb12c95a12
fami1ian <jaykopshlyak@gmail.com>
2025-01-17 15:20:09 +0200

    Playet Rotate

--- Scripts/Player Controller.cs
+++ Scripts/Player Controller.cs
@@ -1,11 +1,19 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
    public float speed;
    public float jumpForce;
    private float moveInput;
+
+   private bool facingRight = true;
+
+   private bool isGrounded;
+   public Transform feetPos;
+   public float checkRadius;
+   public LayerMask whatIsGround;
 
    private Rigidbody2D rb;
        private void Start ()
@@ -16,5 +24,30 @@ public class PlayerController : MonoBehaviour
 {
         moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocityY);
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
 }
+    private void Update ()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = Vector2.up * jumpForce;
+        }
+    }
+
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
 }
